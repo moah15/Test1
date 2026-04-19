4e316cd90e365bcae0e9262d46cf5b2a2a9cb5c1
ST10448258 <165718165+moah15@users.noreply.github.com>
2026-04-19 16:54:40 +0200

    Added reset button feature

--- app/src/main/java/com/example/myapplicationtestforgithub/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationtestforgithub/MainActivity.kt
@@ -1,16 +1,32 @@
 package com.example.myapplicationtestforgithub
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private var count = 0
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val textCount = findViewById<TextView>(R.id.textCount)
+        val buttonReset = findViewById<Button>(R.id.buttonReset)
+
+        buttonReset.setOnClickListener {
+            count = 0
+            textCount.text = count.toString()
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
